dbd449c439429f15113d479fa3847ddfd37e9651
Matheus <matheusousaa@gmail.com>
2025-07-04 00:27:05 -0300

    criado script PHP com Carbon e cURL para contar usuários criados na data atual

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+require 'vendor/autoload.php';
+
+use Carbon\Carbon;
+
 class Users
 {
     private Carbon $carbon;
@@ -12,24 +16,36 @@ class Users
     /** @throws Exception */
     private function request()
     {
-        // Using curl...
-
         $ch = curl_init('https://aj.dev.br/f');
 
-        // ...
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+
+        curl_close($ch);
+
+        $data = json_decode($response, true);
+
+        return $data;
     }
 
     public function getAllCreatedToday()
     {
-        // ...
+        $usuarios = $this->request();
+
+        return array_filter($usuarios, function ($user) {
+            $createdAt = Carbon::parse($user['created_at'])->toDateString();
+            return $createdAt === $this->carbon->toDateString();
+        });
     }
 
     public function getCountCreatedToday()
     {
-        // ...
+        return count($this->getAllCreatedToday());
     }
 }
 
-$users = new Users();
+$users = new Users(Carbon::now());
 
-echo $users->getCountCreatedToday() . PHP_EOL;
+echo "Usuários criados hoje: " . $users->getCountCreatedToday();
